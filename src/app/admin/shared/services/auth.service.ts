14d5e176c7c8bb3fb26f3895574dc12b27d4d6e7
Annnyyy <example@example.com>
2022-02-12 03:00:50 +0200

    #143

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -1,28 +1,67 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { User } from "../interfaces";
+import { catchError, Observable, Subject, tap, throwError } from "rxjs";
+import { environment } from "src/environments/environment";
+import { FbAuthResponse, User } from "../../../shared/interfaces";
 
-@Injectable()
+@Injectable({providedIn: 'root'})
 export class AuthService {
+
+    public error$: Subject<string> = new Subject<string>()
     constructor(private http: HttpClient) {}
 
-    get token(): string {
-        return ''
+    get token(): string | null{
+        const expDate = new Date (localStorage.getItem('fb-token-exp')!)
+        if(new Date() > expDate) {
+            this.logout()
+            return null
+        }
+
+        return localStorage.getItem('fb-token')
     }
 
     login(user: User): Observable<any> {
-        return this.http.post('', user)
+        user.returnSecureToken = true
+        return this.http.post<FbAuthResponse>(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+        .pipe(
+            tap(this.setToken),
+            catchError(this.handelError.bind(this))
+        )
     }
-    logout(user: User) {
-        this.http.post('', user)
-    }
+    logout() {
+        this.setToken(null)
+    }   
 
     isAuthenticated(): boolean {
         return !!this.token
     }
 
-    private setToken() {
+    private handelError(error: HttpErrorResponse) {
+        const {message} = error.error.error
 
+        switch (message) {
+            case 'USER_DISABLED':
+                this.error$.next('Неверный email')
+                break
+            case 'INVALID_PASSWORD':
+                this.error$.next('Неверный пароль')
+                break
+            case 'EMAIL_NOT_FOUND':
+                this.error$.next('Несуществующий email')
+                break
+        }
+
+        return throwError(error)
+    }
+
+    private setToken(response: FbAuthResponse | null) {
+        if(response) {
+            const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000)
+            localStorage.setItem('fb-token', response.idToken)
+            localStorage.setItem('fb-token-exp', expDate.toString())
+        } else {
+            localStorage.clear()
+        }
+        
     }
 }
